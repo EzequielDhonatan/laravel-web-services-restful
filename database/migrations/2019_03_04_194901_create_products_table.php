1ef34541a80084fff2ea12580affaaf30e96c351
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-03-04 19:52:46 -0300

    Model e Table Product

---
+++ database/migrations/2019_03_04_194901_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('name', 100)->unique(); // NAME
+            $table->text('description', 100)->nullable(); // DESCRIPTION
+            $table->string('image')->nullable(); // IMAGE
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}

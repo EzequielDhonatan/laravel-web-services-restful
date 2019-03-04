7e26dd37dbcb1b2e559471ae9209ca3c64c12ed3
Ezequiel Dhonatan <Ezequieldhonatan@gmail.com>
2019-03-04 18:47:18 -0300

    Opção de Filtros em Categorias

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    public function getResults($name = null)
+    {
+        if (!$name)
+            return $this->get();
+
+        return $this->where('name', 'LIKE', "%{$name}%")->get();
+    }
 }
